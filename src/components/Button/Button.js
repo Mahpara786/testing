2c16ff9770d32eeebf022d27076b15d649a86a47
Mahpara786 <mahpara8604@gmail.com>
2021-08-13 15:17:06 +0530

    LearningBasicReactTesting-Intro

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Button() {
+    return (
+        // <div data-testid="button">
+        // {label}
+        // </div>
+        <h2>Welcome</h2>
+        
+    )
+}
+export default Button
